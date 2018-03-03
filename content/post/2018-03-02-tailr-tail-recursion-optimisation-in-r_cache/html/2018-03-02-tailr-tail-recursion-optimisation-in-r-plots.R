bf8c2b494728f2bd95332d0a11ee2fce63386fcb
Thomas Mailund <mailund@birc.au.dk>
2018-03-03 17:56:25 +0100

    updating blog at Sat Mar  3 17:56:25 CET 2018

--- content/post/2018-03-02-tailr-tail-recursion-optimisation-in-r_cache/html/2018-03-02-tailr-tail-recursion-optimisation-in-r-plots.R
+++ content/post/2018-03-02-tailr-tail-recursion-optimisation-in-r_cache/html/2018-03-02-tailr-tail-recursion-optimisation-in-r-plots.R
@@ -1,5 +1,8 @@
-theme_lab <- function () {
-    theme_grey(base_size = 11.5) %+replace%
+
+library(ggplot2)
+
+theme_blog <- function () {
+    theme_gray(base_size = 11.5, base_family = "Roboto") %+replace%
         theme(
             # add padding to the plot
             plot.margin = unit(rep(0.5, 4), "cm"),
@@ -23,30 +26,50 @@ theme_lab <- function () {
             # add padding to the caption
             plot.caption = element_text(size = 10, colour = "#212121", hjust = 1, margin = margin(t = 15)),
             # change to Open Sans for axes titles, tick labels, legend title and legend key, and strip text
-            #axis.title = element_text(family = "Open Sans", size = 11, colour = "#757575", face = "plain", hjust = 1),
-            #axis.text = element_text(family = "Open Sans", size = 10, colour = "#757575", face = "plain"),
+            axis.title = element_text(family = "Open Sans", size = 11, colour = "#757575", face = "plain", hjust = 1),
+            axis.text = element_text(family = "Open Sans", size = 10, colour = "#757575", face = "plain"),
             legend.title = element_text(size = 12, colour = "#757575"),
-            legend.text = element_text(size = 10, colour = "#757575")#,
-            #strip.text = element_text(family = "Open Sans", size = 12, colour = "#757575", face = "plain")
+            legend.text = element_text(size = 10, colour = "#757575"),
+            strip.text = element_text(family = "Open Sans", size = 12, colour = "#757575", face = "plain")
         )
 }
 
+roundUp <- function(x) 10^ceiling(log10(x))
+roundDown <- function(x) 10^floor(log10(x))
+
+log10_ticks <- function(elms) {
+    bottom <- roundDown(min(elms))
+    top <- roundUp(max(elms))
+    orders_of_mag <- log10(top) - log10(bottom)
+    if (orders_of_mag < 0) orders_of_mag <- 1
+
+    # it is slightly easier to compute it this way, where
+    # I have some zeroes when changing order of magnitude
+    # that I can just delete again later
+    ticks <- vector("numeric", length = 10*orders_of_mag)
+    m <- bottom
+    for (i in 1:orders_of_mag) {
+        for (j in 1:9) {
+            ticks[10*(i-1) + j] <- m * j
+        }
+        m <- 10 * m
+    }
+    ticks[10*i] <- m
+    ticks[ticks != 0]
+}
+
+log10_ticks(bm$time)
 
 bm <- microbenchmark::microbenchmark(factorial(n),
                                      loop_factorial(n),
                                      tr_factorial(n))
 
-library(ggplot2)
-ggplot(bm, aes(x = expr, y = time, fill = expr, alpha = 0.8)) + # "#fc6721"
-    scale_fill_brewer(palette = "Set2") +
+ggplot(bm, aes(x = expr, y = time, fill = "#fc6721", alpha = 0.2)) + # "#fc6721"
     geom_boxplot() +
-    scale_y_log10("Microseconds (log-scale)",
-                  breaks = seq(0, 2878054, by = 100000),
-                  labels = seq(0, 2878054, by = 100000)
-    ) +
+    scale_y_log10("Microseconds (log-scale)", breaks = log10_ticks(bm$time)) +
     scale_x_discrete("Function", labels = c("factorial()", "loop_factorial()", "tr_factorial()")) +
     xlab("Function") +
-    theme_lab() + theme(panel.grid.major.x = element_blank(), legend.position = "none")
+    theme_blog() + theme(panel.grid.major.x = element_blank(), legend.position = "none")
 
 
 ggsave("Factorial-running-times.png")
